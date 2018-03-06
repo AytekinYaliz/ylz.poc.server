6accd4ed832bda384baca619ac0aecfa0a57e227
aytekinyaliz <aytekinyaliz@gmail.com>
2018-03-06 09:52:00 +0000

    reoutes

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 
-const port = process.env.PORT || 3000;
 
+/**
+ * App Setup
+ */
 const app = express();
-app.get('/', function(req, res, next) {
-   res.json({ name: 'aytek' });
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+routes(app);
+
+
+/** 
+ * Server Setup
+ * */
+const port = process.env.port || 3001;
+app.listen(port, function() {
+   console.log(`Server listening on port ${port}...`);
 });
-
-app.listen(3000, function() {
-   console.log('Listening on port 3000...');
-
-});
-

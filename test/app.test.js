b1d7436f9e57486530d5d8e1b7caa898d1711212
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2018-07-06 10:56:57 +0100

    update

--- test/app.test.js
+++ test/app.test.js
@@ -1,28 +1,35 @@
+const request = require('supertest');
 const assert = require('assert');
-// const request = require('mocha');
 const chai = require('chai');
 const app = require('./src/app');
 
+before(() => {
+     console.log( 'Once for the file' );
+});
+beforeEach(() => {
+     console.log( 'For each it' );
+});
+
 describe('GET /cities', () => {
+
   it('Test w/ node.assert', (done) => {
        assert.equal(1, 1);
        done();
-
-    // request(app)
-    //   .get('/user')
-    //   .set('Accept', 'application/json')
-    //   .expect('Content-Type', /json/)
-    //   .expect(200, done);
+  });
+  it('Test w/ chai.expect', (done) => {
+       chai.expect(2).to.not.equal(1);
+       done();
   });
 
-  it('Test w/ chai', (done) => {
-       chai.assert.equal(2, 2);
-       done();
-
-    // request(app)
-    //   .get('/user')
-    //   .set('Accept', 'application/json')
-    //   .expect('Content-Type', /json/)
-    //   .expect(200, done);
+  it('Test w/ supertest', (done) => {
+    request(app)
+      .get('/cities')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
   });
 });

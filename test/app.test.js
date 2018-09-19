ae7bb33b5d86f2a57b8e5839c532ceff7da66f30
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2018-09-19 02:18:33 +0100

    chai.request

--- test/app.test.js
+++ test/app.test.js
@@ -1,35 +1,46 @@
-const assert = require('assert');
-const request = require('supertest');
-const chai = require('chai');
-const app = require('./src/app');
+const assert = require("assert");
+const request = require("supertest");
+const chai = require("chai");
+const app = require("./src/app");
 
 before(() => {
-     console.log( 'Once for the file' );
+   console.log("Once for the file");
 });
 beforeEach(() => {
-     console.log( 'For each it' );
+   console.log("For each it");
 });
 
-describe('GET /cities', () => {
+describe("GET /cities", () => {
+   it("Test w/ node.assert", done => {
+      assert.equal(1, 1);
+      done();
+   });
+   it("Test w/ chai.expect", done => {
+      chai.expect(2).to.not.equal(1);
+      done();
+   });
 
-  it('Test w/ node.assert', (done) => {
-       assert.equal(1, 1);
-       done();
-  });
-  it('Test w/ chai.expect', (done) => {
-       chai.expect(2).to.not.equal(1);
-       done();
-  });
+   it("Test w/ chai.request", () => {
+      return chai
+         .request(app)
+         .get("/api/cities")
+         .then(res => {
+            expect(res.status).to.equal(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.an("array");
+            expect(res.body).to.have.length(5);
+         });
+   });
 
-  it('Test w/ supertest', (done) => {
-    request(app)
-      .get('/api/cities')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res) {
-        if (err) return done(err);
-        done();
-      });
-  });
+   it("Test w/ supertest", done => {
+      request(app)
+         .get("/api/cities")
+         .set("Accept", "application/json")
+         .expect("Content-Type", /json/)
+         .expect(200)
+         .end(function(err, res) {
+            if (err) return done(err);
+            done();
+         });
+   });
 });
